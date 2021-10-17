a22e4de8abfc16877128d5adff79ce03385f0a0d
ctroya <carlostroya1@gmail.com>
2021-10-17 16:00:45 -0300

    Linux system commands added

--- shell.py
+++ shell.py
@@ -1,22 +1,30 @@
 from os import close
+from os import system
 from comandos import *
 
 def main():
+    foundCommand = False
     #print(f.readlines())
     while 1:
         a = input("~$ ")
         tokens  = a.split()
         argQuant = len(tokens)
         #print(a.split())
-        if a == "exit":
-            break
-        elif tokens[0] == 'cp':
-            try:
-                cp(tokens[1],tokens[2])
-            except:
-                print("cp: bobito no sabes usar tu comando")
+        for i in commandList: #esto en principio detecta si el usuario ingresa un comando de linux
+            if(tokens[0] == i):
+                foundCommand = True
+        if foundCommand == True:
+            system(a)
         else:
-            print("\""+str(a)+"\" : command not found")
+            if a == "exit":
+                break
+            elif tokens[0] == 'calcecp':
+                try:
+                    cp(tokens[1],tokens[2])
+                except:
+                    print("calcecp: bobito no sabes usar tu comando")
+            else:
+                print("\""+str(a)+"\" : command not found")
     return 0
 
 # la idea es usar un diccionario asi no tengo que usar estos if-elif xD
